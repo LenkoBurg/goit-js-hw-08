cc966b4d31fd678d544597eba4a1c15ac5df8ee2
LenkoBurg <88386193+LenkoBurg@users.noreply.github.com>
2022-01-10 11:58:46 +0000

    Deploying to gh-pages from @ LenkoBurg/goit-js-hw-08@16b8ab5ae7a1b4faebb5a565b486565b02560b76 🚀

--- 01-gallery.068728db.js
+++ 01-gallery.068728db.js
@@ -8,4 +8,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"TfQ5":[function(require,module,exports) {
 "use strict";var e=a(require("simplelightbox"));require("simplelightbox/dist/simple-lightbox.min.css");var l=require("./gallery-items");function a(e){return e&&e.__esModule?e:{default:e}}const i=document.querySelector(".gallery"),r=l.galleryItems.map(e=>`\n        <a class="gallery__item" href="${e.original}">\n            <img class="gallery__image" src="${e.preview}" alt="${e.description}" />\n        </a>\n        `).join("");i.insertAdjacentHTML("beforeend",r);const t=new e.default(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250});
 },{"simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M","./gallery-items":"q5Rn"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/parcel-project-template/01-gallery.068728db.js.map
+//# sourceMappingURL=/goit-js-hw-08/01-gallery.068728db.js.map
